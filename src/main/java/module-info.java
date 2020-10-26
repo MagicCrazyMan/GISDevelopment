cfab527b60f95c99ae8568a5af6d0032ba73c482
magiccrayman <350088648@qq.com>
2020-10-27 00:49:46 +0800

    Add JetBrains Annotations Dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,5 +2,6 @@ module GISDevelopment {
     requires javafx.graphics;
     requires javafx.controls;
     requires com.esri.arcgisruntime;
+    requires org.jetbrains.annotations;
     exports org.example.app to javafx.fxml, javafx.controls, javafx.graphics;
 }
