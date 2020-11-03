32134d3b15183ca54da0238b0ecee886b30909b9
MagicCrazyMan <350088648@qq.com>
2020-11-03 12:26:43 +0800

    Fix runtime problem

--- src/main/java/org/example/app/App.java
+++ src/main/java/org/example/app/App.java
@@ -10,9 +10,10 @@ public class App extends Application {
 
     static {
         String os = System.getProperty("os.name").toLowerCase();
-        if (os.equals("linux")) {
+        System.out.println(os);
+        if (os.startsWith("linux")) {
             ArcGISRuntimeEnvironment.setInstallDirectory("/home/magiccrayman/Downloads/arcgis-runtime-sdk-java-100.9.0");
-        } else if (os.equals("windows")) {
+        } else if (os.startsWith("windows")) {
             ArcGISRuntimeEnvironment.setInstallDirectory("D:\\Data\\Learning\\ArcGISRuntimesSDK\\arcgis-runtime-sdk-java-100.9.0");
         }
     }
